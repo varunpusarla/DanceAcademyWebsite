59d1ecc3578a1148e9df4acc42f33614861e1a44
Varun Pusarla <varunpusarla@gmail.com>
2021-02-22 17:14:22 +0530

    Add backend

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+const bodyparser=require("body-parser");
 const port = 80;
 app.engine('pug', require('pug').__express)
+
+//MONGOOSE
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Define Schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+  });
+
+//compile
+const Contact = mongoose.model('Contact', contactSchema);
+
+//
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -20,8 +38,18 @@ app.get('/', (req, res)=>{
     res.status(200).render('home.pug', params);
 })
 
+app.post('/contact', (req, res)=>{
+    var myData= new Contact(req.body);
+    myData.save().then(()=>{
+        console.log("the data has been saved")
+    }).catch(()=>{
+        res.status(400).send("Not Saved")
+    });
+    res.status(200).render('contact.pug');
+})
+
 app.get('/contact', (req, res)=>{
-    const params = {'title':'hello'}
+    //const params = {'title':'hello'}
     res.status(200).render('contact.pug', params);
 })
 
